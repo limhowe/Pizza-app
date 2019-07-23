964b3b43f4cb20912073271dd5b02a71981b2284
Lim Howe <limhowe1214@gmail.com>
2019-07-23 20:27:03 +0100

    add cart, create pages

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Page from '@containers/layout/Page';
+import PizzaCart from '@containers/PizzaCart';
 
 export default function CartPage() {
-  return <Page title="Cart">Cart!</Page>;
+  return (
+    <Page title="Cart">
+      <PizzaCart />
+    </Page>
+  );
 }
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Page from '@containers/layout/Page';
+import PizzaForm from '@containers/PizzaForm';
 
 export default function CreatePage() {
-  return <Page title="Create">Home!</Page>;
+  return (
+    <Page title="Create">
+      <PizzaForm />
+    </Page>
+  );
 }
